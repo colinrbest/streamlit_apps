b8c92710115b69eeb70358e19d3d2a94fee287fc
Colin Best <colinbest@MacBook-Pro.local>
2022-08-06 16:23:58 -0600

    Generating some diagrams using python diagrams library

---
+++ diagram_generation/GCP_data_collection.py
@@ -0,0 +1,32 @@
+from diagrams import Cluster, Diagram
+from diagrams.gcp.analytics import BigQuery, Dataflow, PubSub, Datalab
+from diagrams.gcp.compute import AppEngine, Functions
+from diagrams.gcp.database import BigTable
+from diagrams.gcp.iot import IotCore
+from diagrams.gcp.storage import GCS
+from diagrams.onprem.analytics import Tableau
+from diagrams.onprem.analytics import Powerbi
+
+
+with Diagram("GCP Services Architecture", show=False):
+    pubsub = PubSub("pubsub")
+
+    with Cluster("Data Generation"):
+        [IotCore("core1"),
+         IotCore("core2"),
+         IotCore("core3")] >> pubsub
+
+    flow = Dataflow("data flow")
+
+    with Cluster("Data Lake"):
+        BQ = BigQuery("bq")
+        flow >> [BQ,
+                 GCS("storage")]
+
+    with Cluster("Flow into any tool for data visualization"):
+        tools = [Datalab("datalab"),
+                 Powerbi("PowerBI"),
+                 Tableau("Tableau")]
+
+    pubsub >> flow
+    BQ >> tools
--- pages/03_Data_Engineering.py
+++ pages/03_Data_Engineering.py
@@ -1,4 +1,13 @@
 import streamlit as st
+from PIL import Image
 
-st.markdown("Data Engineering")
+st.title("Data Engineering")
 st.sidebar.markdown("Data Engineering")
+
+st.write("Architecture I've used in Course Work:")
+st.write("Diagrams generated using diagrams python library")
+gcp_image = Image.open('diagram_generation/gcp_services_architecture.png')
+st.image(gcp_image, width=1000)
+
+
+st.title("Architecture I've used with Clients")

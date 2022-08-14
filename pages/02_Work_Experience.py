f425de7d666afac102300850f1eb53bb1ceb4f0f
colinrbest <colin.best47@gmail.com>
2022-08-13 21:23:42 -0600

    updates to work experience page

--- pages/02_Work_Experience.py
+++ pages/02_Work_Experience.py
@@ -17,7 +17,7 @@ st.sidebar.markdown("Accenture, AT&T, RiskLens")
 st.markdown('''
 ## Work Experience
 ''')
-
+# ACN Experience
 txt('**Data Science Consultant**, Accenture, Denver, Colorado', '2021-Present')
 
 st.markdown('''
@@ -25,19 +25,45 @@ st.markdown('''
 - Worked across several roles as a Data Engineer, Data Scientist, and Team Lead on an Operational Intelligence Platform
 - Click the tabs below to see different tools and architectures used for this work
 ''')
-tab1, tab2, tab3 = st.tabs(
+tab1_acn, tab2_acn, tab3_acn = st.tabs(
     ["Tools Used", "ETL Architecture", "Reporting Architecture"])
 
-with tab1:
+with tab1_acn:
     st.markdown('''
     - ETL: `Python, Linux, Chrontab, Splunk Query Language (SPL), Datadog API, SQL, Kafka`
     - Reporting: `SQL, Python, Jupyter notebooks, Papermill, Chrontab, Seaborn, Streamlit`
     ''')
 
-with tab2:
+with tab2_acn:
     st.header("Example of one ETL process with log records from Splunk")
     st.image(splunk_etl, width=1000)
 
-with tab3:
+with tab3_acn:
+    st.header("Example Daily Report that would run")
+    st.image(reports, width=1000)
+
+# AT&T Experience
+txt('**Data Scientist - Professional Cybersecurity**, AT&T, Dallas, Texas', '2019-2020')
+
+st.markdown('''
+- KRIs, KPIs, and executive vulnerability reporting to the Chief Security Officer
+- UX Analytics to drive cybersecurity product roadmap and feature enhancements
+- VPC Security group recommendation engine for improving security posture
+- Click the tabs below to see different tools and architectures used for this work
+''')
+tab1_att, tab2_att, tab3_att = st.tabs(
+    ["Tools Used", "KRI/KPI Reporting", "UX Analytics", "VPC Rec Engine"])
+
+with tab1_att:
+    st.markdown('''
+    - ETL: `Python, Linux, Chrontab, Splunk Query Language (SPL), Datadog API, SQL, Kafka`
+    - Reporting: `SQL, Python, Jupyter notebooks, Papermill, Chrontab, Seaborn, Streamlit`
+    ''')
+
+with tab2_att:
+    st.header("Example of one ETL process with log records from Splunk")
+    st.image(splunk_etl, width=1000)
+
+with tab3_att:
     st.header("Example Daily Report that would run")
     st.image(reports, width=1000)
